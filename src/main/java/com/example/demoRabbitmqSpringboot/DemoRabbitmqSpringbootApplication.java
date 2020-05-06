55892b21c309e1c8bc4e0e7f18a8c63043df8671
Ramamohan Raju Gadiraju <g.ramamohanraju@gmail.com>
2020-05-06 22:32:53 +0530

    Initial code

---
+++ src/main/java/com/example/demoRabbitmqSpringboot/DemoRabbitmqSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoRabbitmqSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitmqSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabbitmqSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoRabbitmqSpringboot/DemoRabbitmqSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoRabbitmqSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitmqSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
